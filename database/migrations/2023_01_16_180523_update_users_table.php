831cb626cb31754343534823403a9efbdd977b6e
seevrus <tiller2004@gmail.com>
2023-01-16 19:09:19 +0100

    Updated column order in users

---
+++ database/migrations/2023_01_16_180523_update_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('type', 'type_old');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('type', 1)->after('phone_number');
+        });
+
+        DB::table('users')->update([
+            'type' => DB::raw('type_old')
+        ]);
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('type_old');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
