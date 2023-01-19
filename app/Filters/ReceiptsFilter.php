e4436f3cc6255bb2f65dc2dcc268e7aefd72ea9a
seevrus <tiller2004@gmail.com>
2023-01-19 19:51:15 +0100

    Updated receipts filter based on new db scheme

--- app/Filters/ReceiptsFilter.php
+++ app/Filters/ReceiptsFilter.php
@@ -8,11 +8,12 @@ class ReceiptsFilter extends ApiFilter
         'id' => ['eq', 'in'],
         'driverId' => ['eq', 'in'],
         'driverPhoneNumber' => ['eq', 'in'],
-        'clientId' => ['eq', 'in'],
         'roundId' => ['eq', 'in'],
+        'clientId' => ['eq', 'in'],
         'receiptNr' => ['eq', 'lte', 'gte', 'in'],
+        'totalAmount' => ['eq', 'lte', 'gte'],
         'createdAt' => ['lte', 'gte'],
-        'updatedAt' => ['lte', 'gte', 'is_null'],
+        'lastDownloadedAt' => ['lte', 'gte', 'is_null'],
     ];
 
     protected $column_map = [
@@ -22,8 +23,9 @@ class ReceiptsFilter extends ApiFilter
         'clientId' => 'client_id',
         'roundId' => 'round_id',
         'receiptNr' => 'receipt_nr',
+        'totalAmount' => 'total_amount',
         'createdAt' => 'receipts.created_at',
-        'updatedAt' => 'receipts.updated_at',
+        'lastDownloadedAt' => 'receipts.last_downloaded_at',
     ];
 
     protected $operator_map = [
