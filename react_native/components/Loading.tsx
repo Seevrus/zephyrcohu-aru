9f3a5c08d75134cfe250640a64eefaf7015865b3
seevrus <tiller2004@gmail.com>
2023-02-12 09:58:38 +0100

    FE: added an optional message to Loading screen

--- react_native/components/Loading.tsx
+++ react_native/components/Loading.tsx
@@ -1,13 +1,26 @@
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import colors from '../constants/colors';
+import fontSizes from '../constants/fontSizes';
 
-export default function Loading() {
+type LoadingProps = {
+  message?: string;
+};
+
+const defaultProps = {
+  message: '',
+};
+
+export default function Loading({ message }: LoadingProps) {
   return (
     <View style={styles.container}>
       <ActivityIndicator size="large" color={colors.blue200} />
+      <View style={styles.messageContainer}>
+        <Text style={styles.message}>{message}</Text>
+      </View>
     </View>
   );
 }
+Loading.defaultProps = defaultProps;
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +28,15 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  messageContainer: {
+    margin: 30,
+  },
+  message: {
+    color: 'white',
+    fontFamily: 'Muli',
+    fontSize: fontSizes.body,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
