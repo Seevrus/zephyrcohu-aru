54d019d098a27e51db609d70af1890394965c3ed
seevrus <tiller2004@gmail.com>
2023-02-26 01:06:38 +0100

    FE: added mapper to review items before purchase

--- react_native/screens/sell/Review.tsx
+++ react_native/screens/sell/Review.tsx
@@ -1,31 +1,93 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  addIndex,
+  ascend,
+  assoc,
+  flatten,
+  join,
+  keys,
+  map,
+  pathOr,
+  pipe,
+  prop,
+  repeat,
+  sortWith,
+  __,
+} from 'ramda';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Button from '../../components/ui/buttons/Button';
 import colors from '../../constants/colors';
 import fontSizes from '../../constants/fontSizes';
 import { useAppSelector } from '../../store/hooks';
-import { ReviewProps } from '../screen-types';
+import { ExpirationItem, Item } from '../../store/round-slice/round-slice-types';
 
-export default function Review({ navigation }: ReviewProps) {
-  const receipt = useAppSelector((state) => state.round.currentReceipt);
-  console.log(receipt);
+type ReceiptRow = {
+  code: string;
+  name: string;
+  expiresAt: string;
+  quantity: number;
+  unitName: string;
+  netPrice: number;
+  netAmount: number;
+  vatRate: string;
+  vatAmount: number;
+  grossAmount: number;
+};
 
-  const onPressNextHandler = () => {
-    navigation.navigate('Summary');
-  };
+export default function Review() {
+  // TODO: handle the case with only order items present
+  const receiptRows: ReceiptRow = useAppSelector((state) => {
+    const receiptItems = state.round.currentReceipt.items;
+
+    return pipe(
+      pathOr<Item>({}, ['round', 'currentReceipt', 'items']),
+      keys,
+      map((itemId) =>
+        pipe(
+          prop<Record<string, ExpirationItem>>(__, receiptItems),
+          keys,
+          map((expiresAt) => {
+            const item = state.items.data.find((itm) => itm.id === +itemId);
+            const netAmount = item.netPrice * receiptItems[itemId][expiresAt].quantity;
+            const vatRateNumeric = Number.isNaN(+item.vatRate) ? 0 : +item.vatRate;
+            const vatAmount = Math.round(netAmount * (vatRateNumeric / 100));
+
+            return {
+              name: receiptItems[itemId][expiresAt].name,
+              expiresAt,
+              quantity: receiptItems[itemId][expiresAt].quantity,
+              unitName: item.unitName,
+              netPrice: item.netPrice,
+              netAmount,
+              vatRate: item.vatRate,
+              vatAmount,
+              grossAmount: netAmount + vatAmount,
+            };
+          })
+        )(itemId)
+      ),
+      flatten,
+      sortWith([ascend(prop('name')), ascend(prop('expiresAt'))]),
+      addIndex(map)((item, idx) => {
+        const indexLength = String(idx).length;
+        const numLeadingZeros = 3 - indexLength;
+        const code = join('', repeat('0', numLeadingZeros)) + (idx + 1);
+        return assoc('code', code, item);
+      })
+    )(state);
+  });
+
+  console.log(receiptRows);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Áttekintés a partnerről és termékekről. Lehetőség van kikukázni az egészet és visszamenni a
-        kezdőképernyőre, vagy a tovább gombra kattintva véglegesíteni.
-      </Text>
-      <Text style={styles.text}>
-        A Vissza gomb az visszavezet a termékekhez, ha még valamit módosítanának.
-      </Text>
-      <View style={styles.buttonContainer}>
-        <Button variant="ok" onPress={onPressNextHandler}>
-          Tovább
-        </Button>
+      <View style={styles.receiptContainer}>
+        <FlatList />
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.buttonContainer}>
+          <Button variant="ok">Számla készítése</Button>
+          <Button variant="warning">Törlés</Button>
+        </View>
       </View>
     </View>
   );
@@ -36,15 +98,50 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.background,
   },
-  text: {
+
+  title: {
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    textDecorationLine: 'underline',
+    textDecorationColor: 'white',
+  },
+  receiptContainer: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: '7%',
+  },
+  fieldContainer: {
+    marginTop: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  sectionLabel: {
+    fontWeight: '700',
+    textDecorationLine: 'underline',
+    textDecorationColor: 'white',
+  },
+  itemLabel: {
+    fontWeight: '700',
+    marginRight: 5,
+  },
+  receiptText: {
     color: 'white',
-    fontFamily: 'Muli',
-    fontSize: fontSizes.body,
+    fontSize: fontSizes.input,
+  },
+  sectionBorder: {
+    borderColor: 'white',
+    borderWidth: 1,
+    marginVertical: 10,
+  },
+  footerContainer: {
+    height: 50,
+    marginVertical: 10,
   },
   buttonContainer: {
-    marginTop: 30,
-    height: 150,
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: '7%',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
 });
