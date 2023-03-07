9637fe63e4358ec25bef07702aaf1a53df76ff6f
seevrus <tiller2004@gmail.com>
2023-03-07 23:27:05 +0100

    FE: added a mapper in order to display some data in receipt list view (and in details view later)

--- react_native/screens/receipts/ReceiptList.tsx
+++ react_native/screens/receipts/ReceiptList.tsx
@@ -1,13 +1,105 @@
+import {
+  ascend,
+  defaultTo,
+  flatten,
+  keys,
+  map,
+  pipe,
+  prop,
+  reduce,
+  sortWith,
+  when,
+  __,
+} from 'ramda';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from '../../components/ui/Button';
 import colors from '../../constants/colors';
 import fontSizes from '../../constants/fontSizes';
+import { useAppSelector } from '../../store/hooks';
+import { ExpirationItem } from '../../store/round-slice/round-slice-types';
 import { ReceiptListProps } from '../screen-types';
+
+type ReceiptSummaryListItem = {
+  id: number;
+  articleNumber: string;
+  name: string;
+  expiresAt: string; // yyyy-MM
+  quantity: number;
+  unitName: string;
+  grossAmount: number;
+};
+
+type ReceiptSummaryList = {
+  receiptNr: string;
+  deliveryName: string;
+  items: ReceiptSummaryListItem[];
+  total: number;
+};
 
 export default function ReceiptList({ navigation }: ReceiptListProps) {
   const onPressNextHandler = () => {
     navigation.navigate('Receipt');
   };
+
+  const receiptSummaryList: ReceiptSummaryList = useAppSelector((state) => {
+    const { store } = state.stores;
+    const { receipts } = state.round;
+
+    return receipts.map((receipt) => {
+      const partner = state.partners.partners.find((p) => p.id === receipt.partnerId);
+      const priceList = partner?.priceList || {};
+
+      const receiptNr = `${receipt.serialNumber}/${store.yearCode}`;
+      const deliveryName = partner.locations.D?.name;
+      const receiptItems = receipt?.items ?? {};
+      const displayedItems = pipe(
+        keys,
+        map((itemId) =>
+          pipe(
+            prop<Record<string, ExpirationItem>>(__, receiptItems),
+            keys,
+            map((expiresAt) => {
+              const item = state.items.data.find((itm) => itm.id === +itemId);
+              const priceListItem = priceList[item.id];
+              const netPrice = priceListItem?.netPrice || item.netPrice;
+              const netAmount = netPrice * receiptItems[itemId][expiresAt].quantity;
+              const vatRateNumeric = defaultTo(0, +item.vatRate);
+              const vatAmount = Math.round(netAmount * (vatRateNumeric / 100));
+
+              return {
+                id: item.id,
+                articleNumber: item.articleNumber,
+                name: item.name,
+                expiresAt,
+                quantity: receiptItems[itemId][expiresAt].quantity,
+                unitName: item.unitName,
+                grossAmount: netAmount + vatAmount,
+              };
+            })
+          )(itemId)
+        ),
+        flatten,
+        sortWith([ascend(prop('name')), ascend(prop('expiresAt'))])
+      )(receiptItems);
+
+      const total = pipe(
+        reduce<ReceiptSummaryListItem, number>((acc, value) => acc + value.grossAmount, 0),
+        when(
+          () => partner.paymentDays === 0,
+          (t) => Math.round(t / 5) * 5
+        )
+      )(displayedItems);
+
+      return {
+        receiptNr,
+        deliveryName,
+        items: displayedItems,
+        total,
+      };
+    });
+  });
+
+  console.log(receiptSummaryList);
 
   return (
     <View style={styles.container}>
