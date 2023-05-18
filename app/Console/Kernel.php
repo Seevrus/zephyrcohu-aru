e87dd0d7ff0ba066e7a7ad8336137af2875f5793
seevrus <tiller2004@gmail.com>
2023-05-18 23:12:53 +0200

    BE: api config to expire tokens after 25 hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('delete_old_logs')->daily();
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
     }
 
     /**
