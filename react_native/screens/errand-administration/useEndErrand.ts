59ead50022a2418f0c1a7131b5ba795d2b91522e
seevrus <tiller2004@gmail.com>
2023-11-01 09:35:13 +0100

    FE: invalidating queries on finishing round

--- react_native/screens/errand-administration/useEndErrand.ts
+++ react_native/screens/errand-administration/useEndErrand.ts
@@ -1,3 +1,4 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useCallback, useMemo } from 'react';
 
 import { last } from 'ramda';
@@ -5,8 +6,8 @@ import { useFinishRound } from '../../api/mutations/useFinishRound';
 import { useActiveRound } from '../../api/queries/useActiveRound';
 import { useOrdersContext } from '../../providers/OrdersProvider';
 import { useReceiptsContext } from '../../providers/ReceiptsProvider';
-import { useStorageContext } from '../../providers/StorageProvider';
 import { useSellFlowContext } from '../../providers/SellFlowProvider';
+import { useStorageContext } from '../../providers/StorageProvider';
 
 export function useEndErrand() {
   const { isPending: isActiveRoundPending, data: activeRound } =
@@ -23,6 +24,7 @@ export function useEndErrand() {
     useSellFlowContext();
   const { isPending: isStorageContextPending, clearStorageFromContext } =
     useStorageContext();
+  const queryClient = useQueryClient();
 
   const { serialNumber: lastSerialNumber, yearCode } = last(receipts);
 
@@ -33,17 +35,29 @@ export function useEndErrand() {
       yearCode,
     });
 
-    return Promise.all([
+    await Promise.all([
       clearStorageFromContext(),
       resetOrdersContext(),
       resetReceiptsContext(),
       resetSellFlowContext(),
     ]);
+
+    queryClient.invalidateQueries({ queryKey: ['active-round'] });
+    queryClient.invalidateQueries({ queryKey: ['check-token'] });
+    queryClient.invalidateQueries({ queryKey: ['items'] });
+    queryClient.invalidateQueries({ queryKey: ['other-items'] });
+    queryClient.invalidateQueries({ queryKey: ['partner-lists'] });
+    queryClient.invalidateQueries({ queryKey: ['partners'] });
+    queryClient.invalidateQueries({ queryKey: ['price-lists'] });
+    queryClient.invalidateQueries({ queryKey: ['search-tax-number'] });
+    queryClient.invalidateQueries({ queryKey: ['store-details'] });
+    queryClient.invalidateQueries({ queryKey: ['stores'] });
   }, [
     activeRound?.id,
     clearStorageFromContext,
     finishRoundAPI,
     lastSerialNumber,
+    queryClient,
     resetOrdersContext,
     resetReceiptsContext,
     resetSellFlowContext,
