c45f6e68abf6ce58ad91a9fec2a2f196603f11c3
seevrus <tiller2004@gmail.com>
2024-03-18 15:03:11 +0100

    BE: fix round resource format error

--- app/Http/Resources/RoundResource.php
+++ app/Http/Resources/RoundResource.php
@@ -22,8 +22,8 @@ class RoundResource extends JsonResource
             'partnerListId' => $this->partner_list_id,
             'lastSerialNumber' => $this->last_serial_number,
             'yearCode' => $this->year_code,
-            'roundStarted' => Carbon::createFromFormat('Y-m-d H:i:s', $this->round_started),
-            'roundFinished' => $this->round_finished ? Carbon::createFromFormat('Y-m-d H:i:s', $this->round_finished) : null,
+            'roundStarted' => $this->round_started,
+            'roundFinished' => $this->round_finished ? $this->round_finished : null,
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];
