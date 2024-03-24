ebe4bf431b49c106b0e4a8e4b687a326c69bb0bd
seevrus <tiller2004@gmail.com>
2024-03-24 11:38:22 +0100

    added an atom with media storage

--- react_native/atoms/helpers.ts
+++ react_native/atoms/helpers.ts
@@ -1,4 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  EncodingType,
+  cacheDirectory,
+  getInfoAsync,
+  makeDirectoryAsync,
+  readAsStringAsync,
+  writeAsStringAsync,
+} from 'expo-file-system';
+import {
+  addAssetsToAlbumAsync,
+  createAlbumAsync,
+  createAssetAsync,
+  deleteAssetsAsync,
+  getAlbumAsync,
+  getAssetsAsync,
+  getPermissionsAsync,
+  requestPermissionsAsync,
+} from 'expo-media-library';
 import * as SecureStore from 'expo-secure-store';
 import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 
@@ -15,5 +33,111 @@ const SecureStorage = <const>{
 
 export const atomWithSecureStorage = <T>(key: string, content: T) => {
   const storage = createJSONStorage<T>(() => SecureStorage);
+  return atomWithStorage<T>(key, content, storage);
+};
+
+const albumName = 'boreal';
+const storageDirectory = cacheDirectory + albumName;
+const imageExtension = '.jpg';
+
+async function ensureCacheDirectoryExists() {
+  const directoryInfo = await getInfoAsync(storageDirectory);
+
+  if (!directoryInfo.exists) {
+    await makeDirectoryAsync(storageDirectory, {
+      intermediates: true,
+    });
+  }
+}
+
+async function requestPermissions() {
+  let permissions = await getPermissionsAsync();
+
+  if (!permissions.granted && permissions.canAskAgain) {
+    permissions = await requestPermissionsAsync();
+  }
+
+  return permissions;
+}
+
+const MediaStorage = <const>{
+  async getItem(key: string) {
+    const permissions = await requestPermissions();
+
+    if (!permissions.granted) {
+      return;
+    }
+
+    const cacheAlbum = await getAlbumAsync(albumName);
+
+    if (!cacheAlbum) {
+      return;
+    }
+
+    const cacheItems = await getAssetsAsync({ album: cacheAlbum, first: 100 });
+    const cacheItem = cacheItems.assets.find((asset) =>
+      asset.filename.includes(key)
+    );
+
+    if (!cacheItem) {
+      return;
+    }
+
+    const cacheItemUri = cacheItem.uri;
+    const cacheItemContent = await readAsStringAsync(cacheItemUri, {
+      encoding: EncodingType.UTF8,
+    });
+
+    return JSON.parse(cacheItemContent);
+  },
+  async setItem(key: string, value: string) {
+    const permissions = await requestPermissions();
+
+    if (!permissions.granted) {
+      return;
+    }
+
+    await ensureCacheDirectoryExists();
+    const temporaryFile = storageDirectory + '/' + key + imageExtension;
+
+    await writeAsStringAsync(temporaryFile, value, {
+      encoding: EncodingType.UTF8,
+    });
+
+    const cacheItem = await createAssetAsync(temporaryFile);
+    const cacheAlbum = await getAlbumAsync(albumName);
+
+    await (cacheAlbum
+      ? addAssetsToAlbumAsync([cacheItem], cacheAlbum, false)
+      : createAlbumAsync(albumName, cacheItem, false));
+  },
+  async removeItem(key: string) {
+    const permissions = await requestPermissions();
+
+    if (!permissions.granted) {
+      return;
+    }
+
+    const cacheAlbum = await getAlbumAsync(albumName);
+
+    if (!cacheAlbum) {
+      return;
+    }
+
+    const cacheItems = await getAssetsAsync({ album: cacheAlbum, first: 100 });
+    const cacheItem = cacheItems.assets.find((asset) =>
+      asset.filename.includes(key)
+    );
+
+    if (!cacheItem) {
+      return;
+    }
+
+    await deleteAssetsAsync([cacheItem]);
+  },
+};
+
+export const atomWithMediaStorage = <T>(key: string, content: T) => {
+  const storage = createJSONStorage<T>(() => MediaStorage);
   return atomWithStorage<T>(key, content, storage);
 };
